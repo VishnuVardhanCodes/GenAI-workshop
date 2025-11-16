e8b4cbbee39fd67fe0130cdcabada9831bba5af0
POLLA VISHNU VARDHAN <vishnumaxpolla32@gmail.com>
2025-11-16 10:29:59 +0530

    Update persist_pdfs_to_vector_db.py

--- Project_root/persist_pdfs_to_vector_db.py
+++ Project_root/persist_pdfs_to_vector_db.py
@@ -1,3 +1,4 @@
+# here we are importing the all the lanchanin libraries 
 import os
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
